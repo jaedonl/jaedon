ac5b59cc1f3d53e5ac82632a0078e2b255893065
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-01-18 18:20:19 -0500

    -

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './About.scss';
-import { ArrowDownward } from '@material-ui/icons';
 
 const About = () => {
     return (
@@ -13,9 +12,7 @@ const About = () => {
                 <p className="skills">React.js / Shopify Liquid / Javascript / CSS</p>
                 <p className="aboutMe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius quam urna, ac mollis felis rhoncus non. Aliquam volutpat ultrices ultrices. Nam tempor, nunc quis varius cursus, libero nulla convallis felis, id egestas libero leo eu dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum lorem lacus, a consectetur lacus interdum vitae.</p>                
             </div>
-            <div className="aboutArrowDown">
-                <ArrowDownward style={{fontSize: "80px"}} />
-            </div>
+            
         </div>
     )
 }
